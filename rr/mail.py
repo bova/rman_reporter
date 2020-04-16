97c473fd58d393436df1346b5c291fffa67082c5
bova <povetkin84@gmail.com>
2020-04-16 13:13:42 +0500

    added date to attached file name, allow sending mail without authentication

--- rr/mail.py
+++ rr/mail.py
@@ -1,6 +1,9 @@
 import smtplib, ssl
 from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
+from email import encoders
+import io
 
 import rr.conf
 
@@ -10,8 +13,8 @@ class Mail:
     port = ''
     user = ''
     password = ''
-    msg_from = ''
-    msg_to = ''
+    mail_from = ''
+    mail_to = ''
 
     def __init__(self, cfg):
         self.cfg = rr.conf.AppConf()
@@ -22,22 +25,45 @@ class Mail:
         self.password = self.cfg.mail.password
         self.mail_from = self.cfg.mail.mail_from
         self.mail_to = self.cfg.mail.mail_to
+
+        self.msg = MIMEMultipart()
+        # attachments: array of tuples (file_name, file)
+        self.attachments = []
         context = ssl.create_default_context()
         self.mailer = smtplib.SMTP_SSL(host=self.host, port=self.port, context=context)
 
+    def add_attachment_from_string(self, file_name, file_as_string):
+        file = file_as_string.encode(encoding='utf-8')
+        attachment = (file_name, file)
+        self.attachments.append(attachment)
+
+    def attach_files_to_message(self):
+        for file_name, file in self.attachments:
+            part = MIMEBase("application", "octet-stream")
+            part.set_payload(file)
+            encoders.encode_base64(part)
+            part.add_header(
+                "Content-Disposition",
+                f"attachment; filename= {file_name}",
+            )
+            self.msg.attach(part)
+
     def connect(self):
-        self.mailer.login(self.user, self.password)
+        if self.cfg.mail.user and self.cfg.mail.password:
+            self.mailer.login(self.user, self.password)
+        else:
+            pass
 
     def create_msg(self, content):
-        msg = MIMEMultipart()
-        msg['From'] = self.mail_from
-        msg['To'] = self.mail_to
-        msg['Subject'] = "Test rman_reporter"
-        msg.attach(MIMEText(content, 'html'))
-        return msg
+        self.msg['From'] = self.mail_from
+        self.msg['To'] = self.mail_to
+        self.msg['Subject'] = "Test rman_reporter"
+        self.msg.attach(MIMEText(content, 'html'))
+        self.attach_files_to_message()
+        return self.msg
 
     def send(self, content):
         self.connect()
         msg = self.create_msg(content)
-        self.mailer.sendmail("@gmail.com", "@gmail.com", msg.as_string())
+        self.mailer.sendmail(self.mail_from, self.mail_to, msg.as_string())
         self.mailer.quit()
--- rr/main.py
+++ rr/main.py
@@ -1,5 +1,14 @@
 from rr import conf, db, html, mail
+from datetime import datetime
+import os
 
+
+def get_file_name_plus_tod(file_name):
+    file_base_name, file_ext = os.path.splitext(file_name)
+    now = datetime.now()
+    tod = now.strftime("%Y%m%d")
+    new_file_name = file_base_name + '_' + tod + file_ext
+    return new_file_name
 
 
 if __name__ == '__main__':
@@ -12,6 +21,7 @@ if __name__ == '__main__':
     backup_summary_list = conn.get_backup_summary()
     backup_detail_dict = conn.get_backup_details()
     msg_content = html.render(backup_summary_list, backup_detail_dict)
+    mailer.add_attachment_from_string(get_file_name_plus_tod('backup_report.html'), msg_content)
     mailer.send(msg_content)
     print(msg_content)
 
