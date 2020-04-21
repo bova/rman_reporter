335a90b5e5e35e9990824cdcdc9cbf4af1ba47e6
bova <povetkin84@gmail.com>
2020-04-21 23:43:54 +0500

    Added setup script

---
+++ rr/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+
+setuptools.setup(
+    name="rr-bova",
+    version="0.1",
+    author="Bova",
+    author_email="povetkin84@gmail.com",
+    description="RMAN Reporter",
+    url="https://github.com/bova/rman_reporter",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
+)
